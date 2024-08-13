d05c89164ea3c821f47773f6bb820f568ff06387
william-ashby <weashby01@gmail.com>
2024-08-13 18:25:14 -0400

    upload

--- cipher.py
+++ cipher.py
@@ -1,1 +1,31 @@
-# add your code here
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def shift(offset):
+
+    message = input()
+    new_message = ''
+
+    for letter in message:
+
+        letter = letter.lower() 
+
+        if letter.isalpha():
+            shift_pos = alphabet.index(letter) + offset
+            new_pos = alphabet[shift_pos]
+            new_message += new_pos
+
+        
+
+        elif ' ' or '/t' or '/n' in letter: 
+            new_message += letter
+
+        elif letter.isnumeric(): 
+            new_message += letter
+
+        else:
+            print("An error took place in recording the message. Check input.\n")
+
+    print(alphabet)
+
+
+shift(-5)
